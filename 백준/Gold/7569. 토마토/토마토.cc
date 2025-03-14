1fa578e82093612ca3e277919cdb9f788dedb9a8
Sang Soo Woo <112996085+wss0702@users.noreply.github.com>
2025-03-14 14:26:11 +0900

    [Gold V] Title: 토마토, Time: 100 ms, Memory: 11904 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,62 +1,69 @@
 #include<iostream>
 #include<algorithm>
+#include<deque>
 #include<queue>
+#include<string>
 #include<tuple>
 using namespace std;
-int dist[103][103][103];
-int board[103][103][103];
+//tuple사용법 어렵네;;
+
+int board[102][102][102];
+
+
 int dx[6] = { 0,0,1,-1,0,0 };
 int dy[6] = { 1,-1,0,0,0,0 };
 int dz[6] = { 0,0,0,0,1,-1 };
-int M, N, H;
-queue<tuple<int, int, int>>Q;
-int main(void) {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cin >> M >> N >> H;
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < N; j++) {
-			for (int k = 0; k < M; k++) {
-				int tomato;
-				cin >> tomato;
-				board[j][k][i] = tomato;
-				if (tomato == 1) {
-					Q.push({ j,k,i });
-				}
-				if (tomato == 0) {
-					dist[j][k][i] = -1;
-				}
-			}
-		}
-	}
-	while (!Q.empty()) {
-		auto cursor = Q.front(); 
-		Q.pop();
+int vis[102][102][102];
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int N, M, H;
+  cin >> N >> M >> H;
+  queue<tuple<int,int,int>>Q;
+  for(int i = 0; i < H; i++){
+    for(int j = 0; j < M; j++){
+      for(int k = 0; k < N; k++){
+        cin >> board[i][j][k];
+        if(board[i][j][k] == 1){
+          Q.push({i,j,k});
+      }
+      if(board[i][j][k] == 0){
+        vis[i][j][k] = -1;
+        }
+      }
+    }
+  }
+    
+    
+  while(!Q.empty()){
+    auto cur = Q.front();
+    Q.pop();
 		int cursorX, cursorY, cursorZ;
-		tie(cursorX, cursorY, cursorZ) = cursor;
-		for (int dir = 0; dir < 6; dir++) {
-			int nx = cursorX + dx[dir];
-			int ny = cursorY + dy[dir];
-			int nz = cursorZ + dz[dir];
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M || nz < 0 || nz >= H) continue;
-			if (dist[nx][ny][nz] >= 0) continue;
-			dist[nx][ny][nz] = dist[cursorX][cursorY][cursorZ] + 1;
-			Q.push({ nx,ny,nz });
-		}
-	}
-	int ans = 0;
-	for (int i = 0; i < H; i++) {
-		for (int j = 0; j < N; j++) {
-			for (int k = 0; k < M; k++) {
-				if (dist[j][k][i] == -1) {
-					cout << -1;
-					return 0;
-				}
-				ans = max(ans, dist[j][k][i]);
-			}
-		}
-	}
-	cout << ans;
-	return 0;
-	
+		tie(cursorX, cursorY, cursorZ) = cur;
+    for(int i = 0; i < 6; i++){
+      int nh,nm,nn;
+      nh = cursorX + dx[i];
+      nm = cursorY + dy[i];
+      nn = cursorZ+ dz[i];
+      if(nh < 0 || nh >= H || nm < 0 || nm >= M || nn < 0 || nn >= N ) continue;
+      if(vis[nh][nm][nn] >= 0 || board[nh][nm][nn] == -1) continue;
+      vis[nh][nm][nn] = vis[cursorX][cursorY][cursorZ] + 1;
+      Q.push({ nh,nm,nn });
+    }
+  }
+  int ans = 0;
+  for(int i = 0; i < H; i++){
+    for(int j = 0; j < M; j++){
+      for(int k = 0; k < N; k++){
+        if(vis[i][j][k] == -1){
+          cout << -1;
+          return 0;
+        }
+        ans = max(ans,vis[i][j][k]);
+      }
+    }
+  }
+  cout << ans;
+  return 0;
 }
